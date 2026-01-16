4de35b74a3a26dbc0ecd7228ff6f93e8bd62a243
Iwunna George <georgeiwunna@gmail.com>
2026-01-16 21:18:02 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       {!isOwnerPath && <Navbar />}
       <div className='min-h-[70vh]'>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="hotelbooking/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
         </Routes>
